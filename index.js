4f0f5ee224d16b629291d44784ed9b7d53373913
Dev Dork <robertcjohnson1984@gmail.com>
2021-02-22 23:50:38 -0700

    got prompt to work finally

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const { prompt } = require("inquirer");
-const { writeFile } = require("fs")
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // array of questions for user
-const questions = [ {
- // # Your Project Title
- type: 'input',
- message: 'Enter project title',
- name: 'title',
- validate: function(title) {
-     if (title) {
-         return true;
-     }
-     return 'A good readme.md has a project title';
- },
-},
-   
-];
+inquirer.prompt([
+    { // # Your Project Title
+        type: 'input',
+        message: "What is your project's name?",
+        name: 'title',
+    },
 
-// function to write README file
-console.log("Let's create a readme.md!")
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+]).then((data) => {
+console.log(data);
+});
