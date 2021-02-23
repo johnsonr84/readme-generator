8c9e55d80a9b93ed89016eaf6fdb4fb0893f102b
Dev Dork <robertcjohnson1984@gmail.com>
2021-02-22 22:39:28 -0700

    add first question and console.log message to ensure node is working in integrated terminal

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-// array of questions for user
-const questions = [
+const { prompt } = require("inquirer");
+const { writeFile } = require("fs")
 
+// array of questions for user
+const questions = [ {
+ // # Your Project Title
+ type: 'input',
+ message: 'Enter project title',
+ name: 'title',
+ validate: function(title) {
+     if (title) {
+         return true;
+     }
+     return 'A good readme.md has a project title';
+ },
+},
+   
 ];
 
 // function to write README file
+console.log("Let's create a readme.md!")
 function writeToFile(fileName, data) {
 }
 
