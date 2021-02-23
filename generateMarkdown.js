4bbf33750d7214734534e49e89eb683aede0a577
Dev Dork <robertcjohnson1984@gmail.com>
2021-02-23 00:14:34 -0700

    add header variable to generateMarkdown function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,9 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
-  
+  const header = "";
+
+  return `# ${data.title} ${header}
+
   `;
   }
   
